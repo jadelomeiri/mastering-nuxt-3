5e60e2fcc37497c98b77ffb0a9151dcd87aec25f
jad-elomeiri <YrxzXdzv7C8EZwzyB77C>
2023-02-07 10:50:38 +0000

    Creating a Login Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-    Hello, Mastering Nuxt 3!
-</template>
+    <NuxtLink
+      class="underline font-medium"
+      :href="firstLesson.path"
+    >
+      Go to first lesson
+    </NuxtLink>
+  </template>
+  
+  <script setup lang="ts">
+  const firstLesson = useFirstLesson();
+  </script>
