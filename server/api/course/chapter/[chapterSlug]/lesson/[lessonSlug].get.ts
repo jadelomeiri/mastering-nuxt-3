6f1e560a011103771fc70653af6a54b57dc4a59f
jad-elomeiri <YrxzXdzv7C8EZwzyB77C>
2023-02-08 18:14:09 +0000

    Fully Typing Our Endpoint

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,29 +1,43 @@
-import course from '~/server/courseData';
-
-export default defineEventHandler((event) => {
-    const { chapterSlug, lessonSlug } = event.context.params;
-
-    const chapter = course.chapters.find(
+import {
+    Course,
+    Chapter,
+    Lesson,
+    LessonWithPath,
+  } from '~/types/course';
+  import course from '~/server/courseData';
+  
+  course as Course;
+  
+  export default defineEventHandler(
+    (event): LessonWithPath => {
+      const { chapterSlug, lessonSlug } =
+        event.context.params;
+  
+      const chapter: Maybe<Chapter> = course.chapters.find(
         (chapter) => chapter.slug === chapterSlug
-    );
-
-    if (!chapter) {
+      );
+  
+      if (!chapter) {
         throw createError({
-            statusCode: 404,
-            message: 'Chapter not found',
+          statusCode: 404,
+          message: 'Chapter not found',
         });
-    }
-
-    const lesson = chapter.lessons.find(
+      }
+  
+      const lesson: Maybe<Lesson> = chapter.lessons.find(
         (lesson) => lesson.slug === lessonSlug
-    );
-
-    if (!lesson) {
+      );
+  
+      if (!lesson) {
         throw createError({
-            statusCode: 404,
-            message: 'Lesson not found',
+          statusCode: 404,
+          message: 'Lesson not found',
         });
+      }
+  
+      return {
+        ...lesson,
+        path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
+      };
     }
-
-    return lesson;
-});
+  );
