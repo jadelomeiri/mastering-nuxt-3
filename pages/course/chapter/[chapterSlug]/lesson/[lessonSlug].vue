020defc44b64d3d10a2477366cc08750982f1190
jad-elomeiri <YrxzXdzv7C8EZwzyB77C>
2023-02-09 10:50:46 +0000

    Basic Data Fetching

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -5,36 +5,24 @@
     </p>
     <h2 class="my-0">{{ lesson.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
-      <NuxtLink
-        v-if="lesson.sourceUrl"
-        class="font-normal text-md text-gray-500"
-        :to="lesson.sourceUrl"
-      >
+      <NuxtLink v-if="lesson.sourceUrl" class="font-normal text-md text-gray-500" :to="lesson.sourceUrl">
         Download Source Code
       </NuxtLink>
-      <NuxtLink
-        v-if="lesson.downloadUrl"
-        class="font-normal text-md text-gray-500"
-        :to="lesson.downloadUrl"
-      >
+      <NuxtLink v-if="lesson.downloadUrl" class="font-normal text-md text-gray-500" :to="lesson.downloadUrl">
         Download Video
       </NuxtLink>
     </div>
-    <VideoPlayer
-      v-if="lesson.videoId"
-      :videoId="lesson.videoId"
-    />
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
     <p>{{ lesson.text }}</p>
-    <LessonCompleteButton
-      :model-value="isLessonComplete"
-      @update:model-value="toggleComplete"
-    />
+    <LessonCompleteButton :model-value="isLessonComplete" @update:model-value="toggleComplete" />
   </div>
 </template>
 
 <script setup>
 const course = useCourse();
 const route = useRoute();
+const { chapterSlug, lessonSlug } = route.params;
+const lesson = await useLesson(chapterSlug, lessonSlug);
 
 definePageMeta({
   middleware: [
@@ -77,12 +65,6 @@ const chapter = computed(() => {
   );
 });
 
-const lesson = computed(() => {
-  return chapter.value.lessons.find(
-    (lesson) => lesson.slug === route.params.lessonSlug
-  );
-});
-
 const title = computed(() => {
   return `${lesson.value.title} - ${course.title}`;
 });
@@ -99,7 +81,7 @@ const isLessonComplete = computed(() => {
 
   if (
     !progress.value[chapter.value.number - 1][
-      lesson.value.number - 1
+    lesson.value.number - 1
     ]
   ) {
     return false;
